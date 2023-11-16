c5a3b6cf5169b6a4fb3101e94d5038b4c0280559
Siddhant Rath <sid@tamu.edu>
2023-11-16 12:09:32 -0600

    Docs

--- src/BuildingBlocks/Daikon.EventStore/Handlers/EventSourcingHandler.cs
+++ src/BuildingBlocks/Daikon.EventStore/Handlers/EventSourcingHandler.cs
@@ -1,10 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+
+/*
+== Overview ==
+The EventSourcingHandler<TAggregate> class, part of the Daikon.EventStore.Handlers namespace, is a generic handler 
+for event sourcing operations in a CQRS (Command Query Responsibility Segregation) architecture. It is designed 
+to work with aggregate roots of type TAggregate, which must inherit from AggregateRoot and have a parameterless constructor.
+
+== Responsibilities ==
+Event Sourcing Operations: This class is primarily responsible for retrieving and saving the state of aggregates through event sourcing.
+Interface Implementation: It implements the IEventSourcingHandler<TAggregate> interface,
+                          ensuring compliance with the defined contract for event sourcing handlers.
+
+== Developer Notes ==
+new() is a constraint that specifies that TAggregate must have a public parameterless constructor. 
+This allows the EventSourcingHandler to create new instances of TAggregate using the new TAggregate() syntax.
+*/
 
 namespace Daikon.EventStore.Handlers
 {
