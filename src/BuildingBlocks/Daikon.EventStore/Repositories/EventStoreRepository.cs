a807279d49c157383787a21bba64643bb79ea74c
Siddhant Rath <sid@tamu.edu>
2023-11-16 13:18:03 -0600

    docs

--- src/BuildingBlocks/Daikon.EventStore/Repositories/EventStoreRepository.cs
+++ src/BuildingBlocks/Daikon.EventStore/Repositories/EventStoreRepository.cs
@@ -1,13 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using CQRS.Core.Domain;
 using CQRS.Core.Event;
 using Daikon.EventStore.Settings;
-using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
+/* 
+== Overview ==
+The EventStoreRepository class, part of the Daikon.EventStore.Repositories namespace, is responsible for 
+interacting with a MongoDB database to store and retrieve event data. 
+It implements the IEventStoreRepository interface, providing asynchronous methods for saving and querying EventModel objects.
+
+== Key Components ==
+IMongoCollection<EventModel> _eventStoreCollection: A MongoDB collection that holds EventModel objects.
+*/
 namespace Daikon.EventStore.Repositories
 {
     public class EventStoreRepository : IEventStoreRepository
@@ -20,17 +25,36 @@ namespace Daikon.EventStore.Repositories
             var database = client.GetDatabase(settings.DatabaseName);
             _eventStoreCollection = database.GetCollection<EventModel>(settings.CollectionName);
         }
+
+        /*
+         FindByAggregateId(Guid aggregateId):
+
+            Asynchronously retrieves a list of EventModel objects from the MongoDB collection based on the specified aggregate identifier.
+            Returns a Task<List<EventModel>> representing the asynchronous operation.
+        */
         public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
         {
             return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId)
                             .ToListAsync().ConfigureAwait(false);
         }
 
+        /*
+         InsertOneAsync(EventModel @event):
+
+            Asynchronously inserts a single EventModel object into the MongoDB collection.
+            Returns a Task representing the asynchronous operation.
+        */
         public async Task SaveAsync(EventModel @event)
         {
             await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
         }
 
+        /*
+         InsertManyAsync(IEnumerable<EventModel> events):
+
+            Asynchronously inserts a batch of EventModel objects into the MongoDB collection.
+            Returns a Task representing the asynchronous operation.
+        */
         public async Task SaveBatchAsync(IEnumerable<EventModel> events)
         {
             if (events == null || !events.Any())
