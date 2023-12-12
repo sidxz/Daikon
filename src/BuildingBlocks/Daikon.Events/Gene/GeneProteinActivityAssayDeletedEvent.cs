e254dbd3e180b359822cf3f74476ae069b9eae89
Saswati Panda <panda.sas20@gmail.com>
2023-12-12 12:53:34 -0600

    added events for Protein Activity Assay

---
+++ src/BuildingBlocks/Daikon.Events/Gene/GeneProteinActivityAssayDeletedEvent.cs
@@ -0,0 +1,15 @@
+
+using CQRS.Core.Event;
+
+namespace Daikon.Events.Gene
+{
+    public class GeneProteinActivityAssayDeletedEvent : BaseEvent
+    {
+        public GeneProteinActivityAssayDeletedEvent() : base(nameof(GeneProteinActivityAssayDeletedEvent))
+        {
+        }
+
+        public Guid GeneId { get; set; }
+        public Guid ProteinActivityAssayId { get; set; }
+    }
+}
---
+++ src/BuildingBlocks/Daikon.Events/Gene/GeneProteinActivityAssayUpdatedEvent.cs
@@ -0,0 +1,26 @@
+
+using CQRS.Core.Domain;
+using CQRS.Core.Event;
+
+namespace Daikon.Events.Gene
+{
+    public class GeneProteinActivityAssayUpdatedEvent : BaseEvent
+    {
+        public GeneProteinActivityAssayUpdatedEvent() : base(nameof(GeneProteinActivityAssayUpdatedEvent))
+        {
+
+        }
+
+        public Guid GeneId { get; set; }
+        public Guid ProteinActivityAssayId { get; set; }
+        public required DVariable<string> Assay { get; set; }
+        public DVariable<string>? Method { get; set; }
+        public DVariable<string>? Throughput { get; set; }
+        public DVariable<string>? PMID { get; set; }
+        public DVariable<string>? Reference { get; set; }
+        public DVariable<string>? URL { get; set; }
+        
+        public DateTime DateUpdated { get; set; }
+
+    }
+}
