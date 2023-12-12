a4f7b3e90d1c694e06befc7ce8dca36cff094e10
Saswati Panda <panda.sas20@gmail.com>
2023-12-12 16:40:34 -0600

    created building blocks events for hypomorphs

---
+++ src/BuildingBlocks/Daikon.Events/Gene/GeneHypomorphDeletedEvent.cs
@@ -0,0 +1,15 @@
+
+using CQRS.Core.Event;
+
+namespace Daikon.Events.Gene
+{
+    public class GeneHypomorphDeletedEvent : BaseEvent
+    {
+        public GeneHypomorphDeletedEvent() : base(nameof(GeneHypomorphDeletedEvent))
+        {
+        }
+
+        public Guid GeneId { get; set; }
+        public Guid HypomorphId { get; set; }
+    }
+}
