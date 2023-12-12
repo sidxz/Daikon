0cd675451722c4f12bae15f29490771a88671419
Siddhant Rath <sid@tamu.edu>
2023-12-11 22:33:15 -0600

    Refactor

--- src/Services/Gene/Gene.Domain/Aggregates/GeneAggregate_Essentiality.cs
+++ src/Services/Gene/Gene.Domain/Aggregates/GeneAggregate_Essentiality.cs
@@ -1,101 +1,13 @@
 
-using CQRS.Core.Domain;
 using Daikon.Events.Gene;
 using Gene.Domain.Entities;
 
 namespace Gene.Domain.Aggregates
 {
-    public class GeneAggregate : AggregateRoot
+    public partial class GeneAggregate
     {
 
-        private bool _active;
-        private string _AccessionNumber;
         private readonly Dictionary<Guid, Essentiality> _essentialities = [];
-        public GeneAggregate()
-        {
-
-        }
-
-        /* New Gene */
-
-        public GeneAggregate(Entities.Gene gene)
-        {
-            _active = true;
-            _id = gene.Id;
-            _AccessionNumber = gene.AccessionNumber;
-
-            RaiseEvent(new GeneCreatedEvent
-            {
-                Name = gene.Name,
-                Id = gene.Id,
-
-                StrainId = gene.StrainId,
-                AccessionNumber = gene.AccessionNumber,
-                Function = gene.Function,
-                Product = gene.Product,
-                FunctionalCategory = gene.FunctionalCategory,
-
-                DateCreated = DateTime.UtcNow
-            });
-        }
-
-        public void Apply(GeneCreatedEvent @event)
-        {
-            _id = @event.Id;
-            _active = true;
-            _AccessionNumber = @event.AccessionNumber;
-        }
-
-        /* Update Gene */
-
-        public void UpdateGene(Entities.Gene gene)
-        {
-            if (!_active)
-            {
-                throw new InvalidOperationException("This gene is deleted.");
-            }
-
-            RaiseEvent(new GeneUpdatedEvent
-            {
-                Id = gene.Id,
-                StrainId = gene.StrainId,
-                Name = gene.Name,
-                AccessionNumber = gene.AccessionNumber,
-                Function = gene.Function,
-                Product = gene.Product,
-                FunctionalCategory = gene.FunctionalCategory,
-
-            });
-        }
-
-        public void Apply(GeneUpdatedEvent @event)
-        {
-            _id = @event.Id;
-        }
-
-        /* Delete Gene */
-        public void DeleteGene(Entities.Gene gene)
-        {
-            if (!_active)
-            {
-                throw new InvalidOperationException("This gene is already deleted.");
-            }
-
-            RaiseEvent(new GeneDeletedEvent
-            {
-                Id = gene.Id,
-                AccessionNumber = gene.AccessionNumber
-            });
-        }
-
-        public void Apply(GeneDeletedEvent @event)
-        {
-            _id = @event.Id;
-            _AccessionNumber = @event.AccessionNumber;
-            _active = false;
-        }
-
-
 
         /* Add Essentiality */
         public void AddEssentiality(Essentiality essentiality)
