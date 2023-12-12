d5a28f0074c7c445d206b87a35ae8e6e38798c98
Saswati Panda <panda.sas20@gmail.com>
2023-12-12 16:40:34 -0600

    created building blocks events for hypomorphs

--- src/BuildingBlocks/Daikon.Events/Gene/GeneHypomorphAddedEvent.cs
+++ src/BuildingBlocks/Daikon.Events/Gene/GeneHypomorphAddedEvent.cs
@@ -1,17 +1,24 @@
 
 using CQRS.Core.Domain;
+using CQRS.Core.Event;
 
-namespace Gene.Domain.Entities
+namespace Daikon.Events.Gene
 {
-    public class Hypomorph : BaseEntity
+    public class GeneHypomorphAddedEvent : BaseEvent
     {
+        public GeneHypomorphAddedEvent() : base(nameof(GeneHypomorphAddedEvent))
+        {
+
+        }
+
 
         public Guid GeneId { get; set; }
         public Guid HypomorphId { get; set; }
         public required DVariable<string> KnockdownStrain { get; set; }
         public DVariable<string>? Phenotype { get; set; }
         public DVariable<string>? Notes { get; set; }
-
+        
+        public DateTime DateCreated { get; set; }
 
     }
 }
