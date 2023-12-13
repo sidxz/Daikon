d6aec4bd94500026bec3ffc939b92c09d642a756
Saswati Panda <panda.sas20@gmail.com>
2023-12-13 10:32:43 -0600

    aggregates for unpublished structural information

---
+++ src/BuildingBlocks/Daikon.Events/Gene/GeneUnpublishedStructuralInformationAddedEvent.cs
@@ -0,0 +1,30 @@
+
+using CQRS.Core.Domain;
+using CQRS.Core.Event;
+
+namespace Daikon.Events.Gene
+{
+    public class GeneUnpublishedStructuralInformationAddedEvent : BaseEvent
+    {
+        public GeneUnpublishedStructuralInformationAddedEvent() : base(nameof(GeneUnpublishedStructuralInformationAddedEvent))
+        {
+
+        }
+
+
+        public Guid GeneId { get; set; }
+        public Guid UnpublishedStructuralInformationId { get; set; }
+        public required DVariable<string> Organization { get; set; }
+        public DVariable<string>? Method { get; set; }
+        public DVariable<string>? Resolution { get; set; }
+
+        public DVariable<string>? Ligands { get; set; }
+        public DVariable<string>? Researcher { get; set; }
+        public DVariable<string>? Reference { get; set; }
+        public DVariable<string>? Notes { get; set; }
+        public DVariable<string>? URL { get; set; }
+        
+        public DateTime DateCreated { get; set; }
+
+    }
+}
