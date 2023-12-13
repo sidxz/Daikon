ac7f1d335503998b986dae0d7a1ca6ae0da80558
Saswati Panda <panda.sas20@gmail.com>
2023-12-13 10:23:29 -0600

    unpublished structural info entity and revision

---
+++ src/Services/Gene/Gene.Domain/Entities/UnpublishedStructuralInformation.cs
@@ -0,0 +1,23 @@
+
+using CQRS.Core.Domain;
+
+namespace Gene.Domain.Entities
+{
+    public class UnpublishedStructuralInformation : BaseEntity
+    {
+
+        public Guid GeneId { get; set; }
+        public Guid UnpublishedStructuralInformationId { get; set; }
+
+        public required DVariable<string> Organization { get; set; }
+        public DVariable<string>? Method { get; set; }
+        public DVariable<string>? Resolution { get; set; }
+        public DVariable<string>? Ligands { get; set; }
+        public DVariable<string>? Researcher { get; set; }
+        public DVariable<string>? Reference { get; set; }
+        public DVariable<string>? Notes { get; set; }
+        public DVariable<string>? URL { get; set; }
+
+
+    }
+}
