f1417f1bafa428eaa101821d84345c71e05e7ae7
Siddhant Rath <sid@tamu.edu>
2023-12-13 10:34:41 -0600

    Target Controller

--- src/Services/Target/Target.API/Controllers/V2/TargetController.cs
+++ src/Services/Target/Target.API/Controllers/V2/TargetController.cs
@@ -4,7 +4,9 @@ using CQRS.Core.Exceptions;
 using CQRS.Core.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Target.Application.Features.Command.DeleteTarget;
 using Target.Application.Features.Command.NewTarget;
+using Target.Application.Features.Command.UpdateTarget;
 using Target.Application.Features.Queries.GetTarget;
 using Target.Application.Features.Queries.GetTarget.ById;
 
@@ -124,6 +126,110 @@ namespace Target.API.Controllers.V2
                 });
             }
 
+
+        }
+
+
+
+        [HttpPut("{id}", Name = "UpdateTarget")]
+        [MapToApiVersion("2.0")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> UpdateTarget(Guid id, UpdateTargetCommand command)
+        {
+            try
+            {
+                command.Id = id;
+                await _mediator.Send(command);
+
+                return StatusCode(StatusCodes.Status200OK, new BaseResponse
+                {
+                    Message = "Target updated successfully",
+                });
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.LogInformation("UpdateTarget: ArgumentNullException {Id}", id);
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+
+            catch (ResourceNotFoundException ex)
+            {
+                _logger.LogInformation("UpdateTarget: Requested Resource Not Found {Id}", id);
+                return NotFound(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client Made a bad request");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "An error occurred while updating the target";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+
+        }
+
+        [HttpDelete("{id}", Name = "DeleteTarget")]
+        [MapToApiVersion("2.0")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteTarget(Guid id)
+        {
+            try
+            {
+                await _mediator.Send(new DeleteTargetCommand { Id = id });
+
+                return StatusCode(StatusCodes.Status200OK, new BaseResponse
+                {
+                    Message = "Target deleted successfully",
+                });
+            }
+
+            catch (ResourceNotFoundException ex)
+            {
+                _logger.LogInformation("DeleteTarget: Requested Resource Not Found {Id}", id);
+                return NotFound(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client Made a bad request");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "An error occurred while deleting the target";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
 
         }
     }
