e9b1b4464f9ad2df29550bd005236e4007eb58f5
Saswati Panda <panda.sas20@gmail.com>
2023-12-15 15:10:17 -0600

    folder structuring

---
+++ src/Services/Screen/Screen.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Screen.Application.Mappings
+{
+    public class MappingProfile
+    {
+        
+    }
+}
