425d543ccaa5470523648dda4e22513b53008d99
Saswati Panda <panda.sas20@gmail.com>
2023-12-15 13:19:09 -0600

    screen entity revisions

---
+++ src/Services/Screen/Screen.Domain/EntityRevisions/HitRevision.cs
@@ -0,0 +1,25 @@
+
+using CQRS.Core.Domain.Historical;
+
+namespace Screen.Domain.EntityRevisions
+{
+    public class HitRevision : BaseVersionEntity
+    {
+        public DVariableHistory<string>? Library { get; set; }
+        public DVariableHistory<string>? Source { get; set; }
+        public DVariableHistory<string>? Method { get; set; }
+        public DVariableHistory<string>? MIC { get; set; }
+        public DVariableHistory<string>? MICUnit { get; set; }
+        public DVariableHistory<string>? MICCondition { get; set; }
+        public DVariableHistory<string>? IC50 { get; set; }
+        public DVariableHistory<string>? IC50Unit { get; set; }
+        public DVariableHistory<int>? ClusterGroup { get; set; }
+        public DVariableHistory<string>? Notes { get; set; }
+
+        public DVariableHistory<int>? Positive { get; set; }
+        public DVariableHistory<int>? Neutral { get; set; }
+        public DVariableHistory<int>? Negative { get; set; }
+        public DVariableHistory<bool>? IsVotingAllowed { get; set; }
+
+    }
+}
---
+++ src/Services/Screen/Screen.Domain/EntityRevisions/ScreenRunRevision.cs
@@ -0,0 +1,26 @@
+
+
+using CQRS.Core.Domain.Historical;
+
+namespace Screen.Domain.EntityRevisions
+{
+    public class ScreenRunRevision : BaseVersionEntity
+    {
+        
+        public DVariableHistory<string> Library { get; set; }
+        public DVariableHistory<string>? Protocol { get; set; }
+        public DVariableHistory<string>? LibrarySize { get; set; }
+        public DVariableHistory<Guid>? Scientist { get; set; }
+        public DVariableHistory<DateTime>? StartDate { get; set; }
+        public DVariableHistory<int>? UnverifiedHitCount { get; set; }
+        public DVariableHistory<double>? HitRate { get; set; }
+        public DVariableHistory<int>? PrimaryHitCount { get; set; }
+        public DVariableHistory<int>? ConfirmedHitCount { get; set; }
+        public DVariableHistory<int>? NoOfCompoundsScreened { get; set; }
+        public DVariableHistory<string>? Concentration { get; set; }
+        public DVariableHistory<string>? ConcentrationUnit { get; set; }
+        public DVariableHistory<string>? Notes { get; set; }
+
+        
+    }
+}
