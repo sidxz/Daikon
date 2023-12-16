f98fe1cd22b0a422c1ea52a90ec2d19a37b50170
Siddhant Rath <sid@tamu.edu>
2023-12-16 11:02:12 -0600

    Repos

--- src/Services/Screen/Screen.Application/Contracts/Persistence/IHitCollectionRepository.cs
+++ src/Services/Screen/Screen.Application/Contracts/Persistence/IHitCollectionRepository.cs
@@ -3,6 +3,14 @@ namespace Screen.Application.Contracts.Persistence
 {
     public interface IHitCollectionRepository
     {
-        
+        Task CreateHitCollection(Domain.Entities.HitCollection screen);
+        Task<Domain.Entities.HitCollection> ReadHitCollectionByName(string name);
+        Task<Domain.Entities.HitCollection> ReadHitCollectionById(Guid id);
+
+        Task<List<Domain.Entities.HitCollection>> GetHitCollectionsList();
+        Task<List<Domain.Entities.HitCollection>> GetHitCollectionsListByStrainId(Guid strainId);
+        Task UpdateHitCollection(Domain.Entities.HitCollection screen);
+        Task DeleteHitCollection(Domain.Entities.HitCollection screen);
+        Task<Domain.EntityRevisions.HitCollectionRevision> GetHitCollectionRevisions(Guid Id);
     }
 }
