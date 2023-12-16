b32f6e9055d945740145e0fc2a1448274c54b938
Siddhant Rath <sid@tamu.edu>
2023-12-16 11:02:12 -0600

    Repos

--- src/Services/Screen/Screen.Application/Contracts/Persistence/IHitRepository.cs
+++ src/Services/Screen/Screen.Application/Contracts/Persistence/IHitRepository.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Screen.Application.Contracts.Persistence
 {
     public interface IHitRepository
     {
-        
+        Task CreateHit(Domain.Entities.Hit screen);
+        Task<Domain.Entities.Hit> ReadHitById(Guid id);
+        Task<List<Domain.Entities.Hit>> GetHitsList();
+        Task<List<Domain.Entities.Hit>> GetHitsListByStrainId(Guid strainId);
+        Task UpdateHit(Domain.Entities.Hit screen);
+        Task DeleteHit(Domain.Entities.Hit screen);
+        Task<Domain.EntityRevisions.HitRevision> GetHitRevisions(Guid Id);
     }
 }
--- src/Services/Screen/Screen.Application/Contracts/Persistence/IScreenRepository.cs
+++ src/Services/Screen/Screen.Application/Contracts/Persistence/IScreenRepository.cs
@@ -3,7 +3,14 @@ namespace Screen.Application.Contracts.Persistence
 {
     public interface IScreenRepository
     {
+        Task CreateScreen(Domain.Entities.Screen screen);
         Task<Domain.Entities.Screen> ReadScreenByName(string name);
         Task<Domain.Entities.Screen> ReadScreenById(Guid id);
+
+        Task<List<Domain.Entities.Screen>> GetScreensList();
+        Task<List<Domain.Entities.Screen>> GetScreensListByStrainId(Guid strainId);
+        Task UpdateScreen(Domain.Entities.Screen screen);
+        Task DeleteScreen(Domain.Entities.Screen screen);
+        Task<Domain.EntityRevisions.ScreenRevision> GetScreenRevisions(Guid Id);
     }
 }
