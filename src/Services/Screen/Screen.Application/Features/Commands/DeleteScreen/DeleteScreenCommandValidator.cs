b15f41adeaf519b6b4de290400313e9ebccf4f9e
Saswati Panda <panda.sas20@gmail.com>
2023-12-18 10:59:06 -0600

    screen handlers

--- src/Services/Screen/Screen.Application/Features/Commands/DeleteScreen/DeleteScreenCommandValidator.cs
+++ src/Services/Screen/Screen.Application/Features/Commands/DeleteScreen/DeleteScreenCommandValidator.cs
@@ -5,7 +5,17 @@ namespace Screen.Application.Features.Commands.DeleteScreen
 {
     public class DeleteScreenCommandValidator : AbstractValidator<DeleteScreenCommand>
     {
-        
+        public DeleteScreenCommandValidator()
+        {
+            RuleFor(t => t.StrainId)
+                .NotEmpty().WithMessage("{StrainId} is required")
+                .NotNull();
+
+            RuleFor(t => t.Name)
+                .NotEmpty().WithMessage("{Name} is required")
+                .NotNull();
+            
+        }
     }
     
 }
---
+++ src/Services/Screen/Screen.Application/Features/Commands/DeleteScreenRun/DeleteScreenRunCommand.cs
@@ -0,0 +1,12 @@
+using CQRS.Core.Command;
+using MediatR;
+
+namespace Screen.Application.Features.Commands.DeleteScreenRun
+{
+    public class DeleteScreenRunCommand: BaseCommand, IRequest<Unit>
+    {
+        public Guid ScreenId { get; set; }
+
+        public Guid ScreenRunId { get; set; }
+    }
+}
