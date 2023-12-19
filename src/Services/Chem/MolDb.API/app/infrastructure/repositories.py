7b5c7e4583c911839319cd3447b4f292af678b82
Siddhant Rath <sid@tamu.edu>
2023-12-19 09:58:41 -0600

    Molecule Service

--- src/Services/Chem/MolDb.API/app/infrastructure/repositories.py
+++ src/Services/Chem/MolDb.API/app/infrastructure/repositories.py
@@ -1,21 +1,26 @@
 from app.infrastructure.database import get_db
 from app.dto.Molecule import Molecule
 
+
 class MoleculeRepository:
     async def add_molecule(
-        name: str, smiles: str, smiles_canonical :str, molecular_weight: float, tpsa: float
-    ):
+        self,
+        name: str,
+        smiles: str,
+        smiles_canonical: str,
+        molecular_weight: float,
+        tpsa: float,
+    ) -> dict:
         # Check if smiles is blank or invalid
         if not smiles:
             raise ValueError("SMILES string is required.")
-        
-        
+
         # Make smiles string uppercase
         smiles = smiles.upper()
-        conn = await get_db()
-        try:
+
+        async with get_db() as conn:
             # Execute the insert query
-            result = await conn.fetch(
+            result = await conn.fetchrow(
                 """
                 INSERT INTO molecules (name, smiles, smiles_canonical, mol, mfp2, molecular_weight, tpsa)
                 VALUES ($1, $2, $3, mol_from_smiles($4::cstring), morganbv_fp(mol_from_smiles($5::cstring)), $6, $7)
@@ -31,17 +36,17 @@ class MoleculeRepository:
             )
 
             # Extract the molecule ID from the result
-            mol_id = result[0]["id"]
-            
+            mol_id = result["id"]
+
             molecule = Molecule()
 
-            return molecule.dump({
-                "id": mol_id,
-                "name": name,
-                "smiles": smiles,
-                "smiles_canonical": smiles_canonical,
-                "molecular_weight": molecular_weight,
-                "tpsa": tpsa,
-            })
-        finally:
-            await conn.close()
+            return molecule.dump(
+                {
+                    "id": mol_id,
+                    "name": name,
+                    "smiles": smiles,
+                    "smiles_canonical": smiles_canonical,
+                    "molecular_weight": molecular_weight,
+                    "tpsa": tpsa,
+                }
+            )
