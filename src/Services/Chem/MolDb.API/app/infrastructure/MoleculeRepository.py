b13d33c180285ec02960f580358eaa186f9afe9d
Siddhant Rath <sid@tamu.edu>
2023-12-22 15:25:25 -0600

    mol repo

--- src/Services/Chem/MolDb.API/app/infrastructure/MoleculeRepository.py
+++ src/Services/Chem/MolDb.API/app/infrastructure/MoleculeRepository.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from app.Infrastructure.Database import GetDbPool
 from app.DTO.Molecule import Molecule
 from app.Core.Contracts.IMoleculeRepository import IMoleculeRepository
@@ -43,8 +44,6 @@ class MoleculeRepository(IMoleculeRepository):
         if not smiles:
             raise ValueError("SMILES string is required.")
 
-        smiles = smiles.upper()
-
         try:
             async with self.db_pool.acquire() as conn:
                 result = await conn.fetchrow(
@@ -77,4 +76,245 @@ class MoleculeRepository(IMoleculeRepository):
 
         except Exception as e:
             logging.error(f"Error adding molecule to the database: {e}")
+            raise
+
+    async def read_molecule_by_smiles(self, smiles: str) -> dict:
+        """
+        Read a molecule from the database by its SMILES representation.
+
+        Args:
+            smiles (str): SMILES representation of the molecule.
+
+        Returns:
+            dict: A dictionary containing the details of the molecule.
+        """
+        if not smiles:
+            raise ValueError("SMILES string is required.")
+
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetchrow(
+                    "SELECT id, name, smiles, smiles_canonical, molecular_weight, tpsa FROM molecules WHERE smiles = $1;",
+                    smiles,
+                )
+
+                molecule = Molecule()
+                return molecule.dump(
+                    {
+                        "id": result["id"],
+                        "name": result["name"],
+                        "smiles": result["smiles"],
+                        "smilesCanonical": result["smiles_canonical"],
+                        "molecularWeight": result["molecular_weight"],
+                        "tpsa": result["tpsa"],
+                    }
+                )
+
+        except Exception as e:
+            logging.error(f"Error reading molecule from the database: {e}")
+            raise
+
+    async def read_molecule(self, id: UUID) -> dict:
+        """
+        Read a molecule from the database by its ID.
+
+        Args:
+            id (UUID): ID of the molecule.
+
+        Returns:
+            dict: A dictionary containing the details of the molecule.
+        """
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetchrow(
+                    "SELECT id, name, smiles, smiles_canonical, molecular_weight, tpsa FROM molecules WHERE id = $1;",
+                    id,
+                )
+
+                molecule = Molecule()
+                return molecule.dump(
+                    {
+                        "id": result["id"],
+                        "name": result["name"],
+                        "smiles": result["smiles"],
+                        "smilesCanonical": result["smiles_canonical"],
+                        "molecularWeight": result["molecular_weight"],
+                        "tpsa": result["tpsa"],
+                    }
+                )
+
+        except Exception as e:
+            logging.error(f"Error reading molecule from the database: {e}")
+            raise
+
+    async def update_molecule(
+        self,
+        id: UUID,
+        name: str,
+        smiles: str,
+        smiles_canonical: str,
+        molecular_weight: float,
+        tpsa: float,
+    ) -> dict:
+        """
+        Update a molecule in the database.
+
+        Args:
+            name (str): Name of the molecule.
+            smiles (str): SMILES representation of the molecule.
+            smiles_canonical (str): Canonical SMILES representation.
+            molecular_weight (float): Molecular weight of the molecule.
+            tpsa (float): Topological Polar Surface Area.
+
+        Returns:
+            dict: A dictionary containing the details of the updated molecule.
+        """
+        if not smiles:
+            raise ValueError("SMILES string is required.")
+
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetchrow(
+                    """
+                    UPDATE molecules
+                    SET smiles = $1, smiles_canonical = $2, mol = mol_from_smiles($3::cstring), mfp2 = morganbv_fp(mol_from_smiles($4::cstring)), molecular_weight = $5, tpsa = $6, name = $7
+                    WHERE id = $8
+                    RETURNING id;
+                    """,
+                    smiles,
+                    smiles_canonical,
+                    smiles,
+                    smiles,
+                    molecular_weight,
+                    tpsa,
+                    name,
+                    id,
+                )
+
+                mol_id = result["id"]
+                molecule = Molecule()
+                return molecule.dump(
+                    {
+                        "id": mol_id,
+                        "name": name,
+                        "smiles": smiles,
+                        "smilesCanonical": smiles_canonical,
+                        "molecularWeight": molecular_weight,
+                        "tpsa": tpsa,
+                    }
+                )
+
+        except Exception as e:
+            logging.error(f"Error updating molecule in the database: {e}")
+            raise
+
+    async def delete_molecule(self, id: UUID) -> dict:
+        """
+        Delete a molecule from the database.
+
+        Args:
+            name (str): Name of the molecule.
+
+        Returns:
+            dict: A dictionary containing the details of the deleted molecule.
+        """
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetchrow(
+                    "DELETE FROM molecules WHERE id = $1 RETURNING id;", id
+                )
+
+                mol_id = result["id"]
+                molecule = Molecule()
+                return molecule.dump({"id": mol_id})
+
+        except Exception as e:
+            logging.error(f"Error deleting molecule from the database: {e}")
+            raise
+
+    async def find_similar_molecule(
+        self, smiles: str, threshold: float = 0.8, num_results: int = 10
+    ) -> list:
+        """
+        Find a similar molecule in the database.
+
+        Args:
+            smiles (str): SMILES representation of the molecule.
+            threshold (float, optional): Similarity threshold. Defaults to 0.8.
+            num_results (int, optional): Number of results to return. Defaults to 10.
+
+        Returns:
+            List of dict: A list of dictionary containing the details of the similar molecule.
+        """
+        if not smiles:
+            raise ValueError("SMILES string is required.")
+
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetch(
+                    """
+                    SELECT id, name, smiles, smiles_canonical, molecular_weight, tpsa
+                    FROM molecules 
+                    WHERE tanimoto_sml(morganbv_fp(mol_from_smiles($1::cstring)), mfp2) >= $2
+                    ORDER BY tanimoto_sml(morganbv_fp(mol_from_smiles($3::cstring)), mfp2) DESC
+                    LIMIT $4;
+                    """,
+                    smiles,
+                    threshold,
+                    smiles,
+                    num_results,
+                )
+
+                molecule = Molecule()
+                return [
+                    molecule.dump(
+                        {
+                            "id": mol["id"],
+                            "name": mol["name"],
+                            "smiles": mol["smiles"],
+                            "smilesCanonical": mol["smiles_canonical"],
+                            "molecularWeight": mol["molecular_weight"],
+                            "tpsa": mol["tpsa"],
+                        }
+                    )
+                    for mol in result
+                ]
+
+        except Exception as e:
+            logging.error(f"Error finding similar molecule in the database: {e}")
+            raise
+
+    async def list_molecules(self) -> dict:
+        """
+        List all molecules in the database.
+
+        Returns:
+            dict: A dictionary containing the details of the molecules.
+        """
+        try:
+            async with self.db_pool.acquire() as conn:
+                result = await conn.fetch(
+                    """
+                    SELECT id, name, smiles, smiles_canonical, molecular_weight, tpsa
+                    FROM molecules;
+                    """
+                )
+
+                molecule = Molecule()
+                return [
+                    molecule.dump(
+                        {
+                            "id": mol["id"],
+                            "name": mol["name"],
+                            "smiles": mol["smiles"],
+                            "smilesCanonical": mol["smiles_canonical"],
+                            "molecularWeight": mol["molecular_weight"],
+                            "tpsa": mol["tpsa"],
+                        }
+                    )
+                    for mol in result
+                ]
+
+        except Exception as e:
+            logging.error(f"Error listing molecules in the database: {e}")
             raise
