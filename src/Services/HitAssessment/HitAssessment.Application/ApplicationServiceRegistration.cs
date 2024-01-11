a5ff196d4751b02400e47d1f87b651a2bb9a2dad
Siddhant Rath <sid@tamu.edu>
2024-01-10 22:19:55 -0600

    API

---
+++ src/Services/HitAssessment/HitAssessment.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,29 @@
+
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using HitAssessment.Application.Features.Commands.NewHitAssessment;
+using HitAssessment.Application.Mappings;
+
+namespace HitAssessment.Application
+{
+    /* Dependency Injection for all services in Ordering.Application */
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+            services.AddMediatR(typeof(NewHitAssessmentCommand).Assembly);
+            services.AddValidatorsFromAssembly(typeof(NewHitAssessmentCommandValidator).Assembly);
+
+            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(Behaviours.UnhandledExceptionBehaviour<,>));
+            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(Behaviours.ValidationBehaviour<,>));
+
+            // services.AddScoped<IHitAssessmentEventHandler, HitAssessmentEventHandler>();
+
+            return services;
+        }
+
+    }
+}
