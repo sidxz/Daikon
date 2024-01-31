1b16c788a3153a125e4321b0f6220e060bef665d
Siddhant Rath <sid@tamu.edu>
2024-01-31 09:58:09 -0600

    Refactor

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -7,56 +7,69 @@ using OcelotApiGw.OIDCProviders;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Configure app configuration
-builder.Configuration.AddJsonFile
-  ($"OcelotGlobalConfig.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-  .AddJsonFile($"Gateways/{builder.Environment.EnvironmentName}/gw.genes.json", optional: false, reloadOnChange: true)
-  .AddJsonFile($"Gateways/{builder.Environment.EnvironmentName}/gw.targets.json", optional: false, reloadOnChange: true);
 
-  
 // Configure logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-MicrosoftEntraID.Configure(builder.Services, builder.Configuration);
+
+// Configure Ocelot configuration.
+
+builder.Configuration.AddJsonFile
+  ($"OcelotGlobalConfig.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+  .AddJsonFile($"Gateways/{builder.Environment.EnvironmentName}/gw.genes.json", optional: false, reloadOnChange: true)
+  .AddJsonFile($"Gateways/{builder.Environment.EnvironmentName}/gw.targets.json", optional: false, reloadOnChange: true);
+
+// Select the OIDC provider based on the settings
+var authProvider = builder.Configuration["OIDCProvider"];
+switch (authProvider)
+{
+  case "KeyCloak":
+    KeyCloak.Configure(builder.Services, builder.Configuration);
+    break;
+  case "EntraID":
+    MicrosoftEntraID.Configure(builder.Services, builder.Configuration);
+    break;
+  default:
+    MicrosoftEntraID.Configure(builder.Services, builder.Configuration);
+    break;
+}
+
 
 // Register a http client for the User Store API
-
 builder.Services.AddHttpClient<IUserStoreAPIService, UserStoreAPIService>();
+
 // Add UserStoreAPIService
 builder.Services.AddScoped<IUserStoreAPIService, UserStoreAPIService>();
 
 // Add Ocelot services
-
 builder.Services.AddOcelot();
 
+// Middlewares
 builder.Services.AddScoped<OAuth2UserAccessHandler>();
-
-
 
 var app = builder.Build();
 
-app.UseAuthentication(); 
+
+// Configure the HTTP request pipeline.
+app.UseAuthentication();
 app.UseAuthorization();
 
 var oAuth2UserAccessHandler = app.Services.GetRequiredService<OAuth2UserAccessHandler>();
 
 var ocelotPipelineConfig = new OcelotPipelineConfiguration
 {
-    AuthorizationMiddleware = async (ctx, next) =>
-    {
-        // Invoke custom middleware logic here
-        await oAuth2UserAccessHandler.ValidateUser(ctx, next);
-    }
+  AuthorizationMiddleware = async (ctx, next) =>
+  {
+    await oAuth2UserAccessHandler.ValidateUser(ctx, next);
+  }
 };
-
-
-
 
 app.MapGet("/", () => "Hello World!");
 
 app.UseOcelot(ocelotPipelineConfig).Wait();
-//app.UseMiddleware<LogAuthHeadersMiddleware>();
+
+
 app.Run();
