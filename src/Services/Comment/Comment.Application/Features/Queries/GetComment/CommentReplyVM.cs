93e8af84d4a683a24c86de13ef2efb9b33a78db1
Siddhant Rath <sid@tamu.edu>
2024-04-26 11:19:51 -0500

    comments fix

--- src/Services/Comment/Comment.Application/Features/Queries/GetComment/CommentReplyVM.cs
+++ src/Services/Comment/Comment.Application/Features/Queries/GetComment/CommentReplyVM.cs
@@ -9,6 +9,7 @@ namespace Comment.Application.Features.Queries.GetComment
     public class CommentReplyVM :DocMetadata
     {
         public Guid Id { get; set; }
+        public Guid CommentId { get; set; }
         public object Body { get; set; }
         public HashSet<string> Tags { get; set; }
         public HashSet<Guid> Mentions { get; set; }
