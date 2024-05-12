9aa9d1b6b39a2fdbc661add9024cedda450b31aa
Siddhant Rath <sid@tamu.edu>
2024-05-12 14:19:24 -0500

    Test with webIdentity

--- src/ApiGateways/SimpleGW/SimpleGW.API/OIDCProviders/MicrosoftEntraID.cs
+++ src/ApiGateways/SimpleGW/SimpleGW.API/OIDCProviders/MicrosoftEntraID.cs
@@ -1,8 +1,7 @@
-
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.Identity.Web;
 
 namespace SimpleGW.OIDCProviders
 {
@@ -11,54 +10,38 @@ namespace SimpleGW.OIDCProviders
         public static void Configure(IServiceCollection services, IConfiguration configuration, ILogger logger)
         {
             // Microsoft Entra ID Configuration
-            var entraIdConfig = configuration.GetSection("EntraID");
+            var entraIdConfig = configuration.GetSection("AzureAd"); // Use AzureAd section as recommended
             if (!entraIdConfig.Exists())
             {
-                logger.LogWarning("EntraID configuration section is missing.");
+                logger.LogWarning("AzureAd configuration section is missing.");
                 return;
             }
 
             /* Dump configuration to console for debugging */
-            Console.WriteLine($"EntraID Instance: {entraIdConfig["Instance"]}");
-            Console.WriteLine($"EntraID Domain: {entraIdConfig["Domain"]}");
-            Console.WriteLine($"EntraID TenantId: {entraIdConfig["TenantId"]}");
-            Console.WriteLine($"EntraID Audience: {entraIdConfig["Audience"]}");
-            Console.WriteLine($"EntraID ClientId: {entraIdConfig["ClientId"]}");
-            Console.WriteLine($"EntraID Issuer: {entraIdConfig["Issuer"]}");
+            Console.WriteLine($"AzureAd Instance: {entraIdConfig["Instance"]}");
+            Console.WriteLine($"AzureAd Domain: {entraIdConfig["Domain"]}");
+            Console.WriteLine($"AzureAd TenantId: {entraIdConfig["TenantId"]}");
+            Console.WriteLine($"AzureAd Audience: {entraIdConfig["Audience"]}");
+            Console.WriteLine($"AzureAd ClientId: {entraIdConfig["ClientId"]}");
+            Console.WriteLine($"AzureAd Issuer: {entraIdConfig["Issuer"]}");
 
             var requiredConfigs = new[] { "Instance", "Domain", "TenantId", "Audience", "ClientId" };
             foreach (var configKey in requiredConfigs)
             {
                 if (string.IsNullOrEmpty(entraIdConfig[configKey]))
                 {
-                    logger.LogWarning($"EntraID configuration key '{configKey}' is missing or empty.");
-                    throw new InvalidOperationException($"EntraID configuration key '{configKey}' is missing or empty.");
+                    logger.LogWarning($"AzureAd configuration key '{configKey}' is missing or empty.");
+                    throw new InvalidOperationException($"AzureAd configuration key '{configKey}' is missing or empty.");
                 }
             }
+
+            // Configure Microsoft Identity Web to use Azure AD
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+                    .AddMicrosoftIdentityWebApi(configuration, "AzureAd");
+
+            // Configure event handlers and default authorization policy
+            services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
             {
-                configuration.Bind(options);
-                options.Authority = $"{entraIdConfig["Instance"]}{entraIdConfig["TenantId"]}/v2.0/";
-                options.Audience = entraIdConfig["Audience"];
-                options.TokenValidationParameters.ValidAudience = entraIdConfig["ClientId"];
-                // options.TokenValidationParameters.ValidateAudience = false;
-                // Enable issuer validation explicitly
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = entraIdConfig["Issuer"],
-                    ValidateAudience = true,
-                    ValidAudience = entraIdConfig["Audience"],
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ClockSkew = TimeSpan.Zero
-                };
-
-                Console.WriteLine("EntraID Configuration: {options}");
-                Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(options));
-
-
                 options.Events = new JwtBearerEvents
                 {
                     OnAuthenticationFailed = context =>
@@ -79,7 +62,7 @@ namespace SimpleGW.OIDCProviders
                             }
                         }
                         return Task.CompletedTask;
-                    },
+                    }
                 };
             });
 
@@ -87,7 +70,7 @@ namespace SimpleGW.OIDCProviders
             {
                 options.DefaultPolicy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
-                    .AddAuthenticationSchemes("EntraID")
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
                     .Build();
             });
         }
---
+++ src/ApiGateways/SimpleGW/SimpleGW.API/OIDCProviders/MicrosoftEntraID_JWT.cs
@@ -0,0 +1,95 @@
+
+// using System.Security.Claims;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.IdentityModel.Tokens;
+
+// namespace SimpleGW.OIDCProviders
+// {
+//     public static class MicrosoftEntraID
+//     {
+//         public static void Configure(IServiceCollection services, IConfiguration configuration, ILogger logger)
+//         {
+//             // Microsoft Entra ID Configuration
+//             var entraIdConfig = configuration.GetSection("EntraID");
+//             if (!entraIdConfig.Exists())
+//             {
+//                 logger.LogWarning("EntraID configuration section is missing.");
+//                 return;
+//             }
+
+//             /* Dump configuration to console for debugging */
+//             Console.WriteLine($"EntraID Instance: {entraIdConfig["Instance"]}");
+//             Console.WriteLine($"EntraID Domain: {entraIdConfig["Domain"]}");
+//             Console.WriteLine($"EntraID TenantId: {entraIdConfig["TenantId"]}");
+//             Console.WriteLine($"EntraID Audience: {entraIdConfig["Audience"]}");
+//             Console.WriteLine($"EntraID ClientId: {entraIdConfig["ClientId"]}");
+//             Console.WriteLine($"EntraID Issuer: {entraIdConfig["Issuer"]}");
+
+//             var requiredConfigs = new[] { "Instance", "Domain", "TenantId", "Audience", "ClientId" };
+//             foreach (var configKey in requiredConfigs)
+//             {
+//                 if (string.IsNullOrEmpty(entraIdConfig[configKey]))
+//                 {
+//                     logger.LogWarning($"EntraID configuration key '{configKey}' is missing or empty.");
+//                     throw new InvalidOperationException($"EntraID configuration key '{configKey}' is missing or empty.");
+//                 }
+//             }
+//             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//             .AddJwtBearer(options =>
+//             {
+//                 configuration.Bind(options);
+//                 options.Authority = $"{entraIdConfig["Instance"]}{entraIdConfig["TenantId"]}/v2.0/";
+//                 options.Audience = entraIdConfig["Audience"];
+//                 options.TokenValidationParameters.ValidAudience = entraIdConfig["ClientId"];
+//                 // options.TokenValidationParameters.ValidateAudience = false;
+//                 // Enable issuer validation explicitly
+//                 options.TokenValidationParameters = new TokenValidationParameters
+//                 {
+//                     ValidateIssuer = true,
+//                     ValidIssuer = entraIdConfig["Issuer"],
+//                     ValidateAudience = true,
+//                     ValidAudience = entraIdConfig["Audience"],
+//                     ValidateLifetime = true,
+//                     ValidateIssuerSigningKey = true,
+//                     ClockSkew = TimeSpan.Zero
+//                 };
+
+//                 Console.WriteLine("EntraID Configuration: {options}");
+//                 Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(options));
+
+
+//                 options.Events = new JwtBearerEvents
+//                 {
+//                     OnAuthenticationFailed = context =>
+//                     {
+//                         logger.LogError($"Authentication failed: {context.Exception}");
+//                         return Task.CompletedTask;
+//                     },
+//                     OnTokenValidated = context =>
+//                     {
+//                         logger.LogDebug("Token validated.");
+//                         var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+//                         if (claimsIdentity != null)
+//                         {
+//                             logger.LogDebug($"ClaimsIdentity is authenticated: {claimsIdentity.IsAuthenticated}");
+//                             foreach (var claim in claimsIdentity.Claims)
+//                             {
+//                                 Console.WriteLine($"Claim type: {claim.Type}, value: {claim.Value}");
+//                             }
+//                         }
+//                         return Task.CompletedTask;
+//                     },
+//                 };
+//             });
+
+//             services.AddAuthorization(options =>
+//             {
+//                 options.DefaultPolicy = new AuthorizationPolicyBuilder()
+//                     .RequireAuthenticatedUser()
+//                     .AddAuthenticationSchemes("EntraID")
+//                     .Build();
+//             });
+//         }
+//     }
+// }
