f8d9e92f1a74a64f69dd42a4b0eb7f8070d46559
Siddhant Rath <sid@tamu.edu>
2024-08-29 16:37:26 -0500

    compound associations

---
+++ src/Services/Horizon/Horizon.Application/Features/Queries/CompoundRelations/CompoundRelationsQuery.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Query;
+using MediatR;
+
+namespace Horizon.Application.Features.Queries.CompoundRelations
+{
+    public class CompoundRelationsQuery : BaseQuery, IRequest<List<CompoundRelationsVM>>
+    {
+        public Guid Id { get; set; }
+    }
+}
