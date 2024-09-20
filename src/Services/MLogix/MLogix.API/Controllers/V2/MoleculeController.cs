481d60847d7609f2193d5e173ada9ad7c5a489cc
Siddhant Rath <sid@tamu.edu>
2024-09-20 12:50:55 -0500

    API Error Middleware : MLogix

--- src/Services/MLogix/MLogix.API/Controllers/V2/MoleculeController.cs
+++ src/Services/MLogix/MLogix.API/Controllers/V2/MoleculeController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using System.Net;
-using System.Threading.Tasks;
-using CQRS.Core.Exceptions;
-using CQRS.Core.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MLogix.Application.Features.Commands.RegisterMolecule;
@@ -19,38 +14,19 @@ namespace MLogix.API.Controllers.V2
     [ApiController]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("2.0")]
-    public partial class MoleculeController : ControllerBase
+    public partial class MoleculeController(IMediator mediator) : ControllerBase
     {
-        private readonly IMediator _mediator;
-        private readonly ILogger<MoleculeController> _logger;
-
-        public MoleculeController(IMediator mediator, ILogger<MoleculeController> logger)
-        {
-            _mediator = mediator;
-            _logger = logger;
-        }
+        private readonly IMediator _mediator = mediator;
 
         [HttpGet(Name = "GetAllMolecules")]
         [MapToApiVersion("2.0")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetAllMolecules()
         {
-            try
-            {
-                var query = new ListMoleculesCommand { };
-                var molecules = await _mediator.Send(query);
-                return Ok(molecules);
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while fetching the molecules";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            var query = new ListMoleculesCommand { };
+            var molecules = await _mediator.Send(query);
+            return Ok(molecules);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
         [HttpGet("{id}", Name = "GetMoleculeById")]
@@ -59,30 +35,10 @@ namespace MLogix.API.Controllers.V2
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> GetMoleculeById(Guid id, [FromQuery] bool WithMeta = false)
         {
-            try
-            {
-                var query = new GetMoleculeByIdQuery { Id = id, WithMeta = WithMeta };
-                var molecule = await _mediator.Send(query);
-                return Ok(molecule);
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                _logger.LogInformation("GetMoleculeById: Resource Not Found {Id}", id);
-                return NotFound(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while fetching the molecule";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            var query = new GetMoleculeByIdQuery { Id = id, WithMeta = WithMeta };
+            var molecule = await _mediator.Send(query);
+            return Ok(molecule);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
         [HttpGet("by-smiles/{smiles}", Name = "GetMoleculeBySMILES")]
@@ -91,30 +47,10 @@ namespace MLogix.API.Controllers.V2
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> GetMoleculeBySMILES(string smiles)
         {
-            try
-            {
-                var query = new GetMoleculeBySMILESQuery { SMILES = smiles };
-                var molecule = await _mediator.Send(query);
-                return Ok(molecule);
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                _logger.LogInformation("GetMoleculeBySMILES: Resource Not Found {SMILES}", smiles);
-                return NotFound(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while fetching the molecule";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            var query = new GetMoleculeBySMILESQuery { SMILES = smiles };
+            var molecule = await _mediator.Send(query);
+            return Ok(molecule);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
 
@@ -124,30 +60,10 @@ namespace MLogix.API.Controllers.V2
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> GetMoleculeByRegistrationId(Guid regId)
         {
-            try
-            {
-                var query = new GetMoleculeByRegIdQuery { RegistrationId = regId };
-                var molecule = await _mediator.Send(query);
-                return Ok(molecule);
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                _logger.LogInformation("GetMoleculeByRegistrationId: Resource Not Found {Id}", regId);
-                return NotFound(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while fetching the molecule";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            var query = new GetMoleculeByRegIdQuery { RegistrationId = regId };
+            var molecule = await _mediator.Send(query);
+            return Ok(molecule);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
         [HttpGet("similar/{smiles}", Name = "FindSimilarMolecules")]
@@ -156,30 +72,10 @@ namespace MLogix.API.Controllers.V2
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> FindSimilarMolecules(string smiles, [FromQuery] double similarityThreshold = 1, [FromQuery] int maxResults = 10, [FromQuery] bool WithMeta = false)
         {
-            try
-            {
-                var query = new FindSimilarMoleculesQuery { SMILES = smiles, SimilarityThreshold = similarityThreshold, MaxResults = maxResults, WithMeta = WithMeta };
-                var molecules = await _mediator.Send(query);
-                return Ok(molecules);
-            }
-            catch (ResourceNotFoundException ex)
-            {
-                _logger.LogInformation("FindSimilarMolecules: Resource Not Found {SMILES}", smiles);
-                return NotFound(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while fetching the molecule";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            var query = new FindSimilarMoleculesQuery { SMILES = smiles, SimilarityThreshold = similarityThreshold, MaxResults = maxResults, WithMeta = WithMeta };
+            var molecules = await _mediator.Send(query);
+            return Ok(molecules);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
         [HttpPost(Name = "RegisterMolecule")]
@@ -188,49 +84,10 @@ namespace MLogix.API.Controllers.V2
         public async Task<IActionResult> RegisterMolecule([FromBody] RegisterMoleculeCommand command)
         {
             var id = Guid.NewGuid();
-            try
-            {
-                command.Id = id;
-                RegisterMoleculeResponseDTO registerMoleculeResponseDTO = await _mediator.Send(command);
-                return StatusCode(StatusCodes.Status201Created, registerMoleculeResponseDTO);
-            }
-            catch (ArgumentNullException ex)
-            {
-                _logger.LogInformation("RegisterMolecule: ArgumentNullException {Id}", id);
-                return BadRequest(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
+            command.Id = id;
+            RegisterMoleculeResponseDTO registerMoleculeResponseDTO = await _mediator.Send(command);
+            return StatusCode(StatusCodes.Status201Created, registerMoleculeResponseDTO);
 
-            catch (DuplicateEntityRequestException ex)
-            {
-                _logger.LogInformation("RegisterMolecule: Requested Resource Already Exists {Name}", ex.Message);
-                return Conflict(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (InvalidOperationException ex)
-            {
-                _logger.Log(LogLevel.Warning, ex, "Client Made a bad request");
-                return BadRequest(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while adding the screen";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
-
-                return StatusCode(StatusCodes.Status500InternalServerError, new AddResponse
-                {
-                    Id = id,
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
 
         [HttpPut("{id}", Name = "UpdateMolecule")]
@@ -238,38 +95,10 @@ namespace MLogix.API.Controllers.V2
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> UpdateMolecule(Guid id, [FromBody] UpdateMoleculeCommand command)
         {
-            try
-            {
-                command.Id = id;
-                var updateMoleculeResponseDTO = await _mediator.Send(command);
-                return Ok(updateMoleculeResponseDTO);
-            }
-            catch (ArgumentNullException ex)
-            {
-                _logger.LogInformation("UpdateMolecule: ArgumentNullException {Id}", id);
-                return BadRequest(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (InvalidOperationException ex)
-            {
-                _logger.Log(LogLevel.Warning, ex, "Client Made a bad request");
-                return BadRequest(new BaseResponse
-                {
-                    Message = ex.Message
-                });
-            }
-            catch (Exception ex)
-            {
-                const string SAFE_ERROR_MESSAGE = "An error occurred while updating the molecule";
-                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+            command.Id = id;
+            var updateMoleculeResponseDTO = await _mediator.Send(command);
+            return Ok(updateMoleculeResponseDTO);
 
-                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
-                {
-                    Message = SAFE_ERROR_MESSAGE
-                });
-            }
         }
     }
 }
