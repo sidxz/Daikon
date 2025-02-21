f4e4058b4d49347a2ab4b5ab4b668f016594368d
Siddhant Rath <sid@tamu.edu>
2025-02-21 09:58:44 -0600

    fix to update screen modified date on screen run added event

--- src/Services/Screen/Screen.Application/EventHandlers/ScreenEventHandler_ScreenRun.cs
+++ src/Services/Screen/Screen.Application/EventHandlers/ScreenEventHandler_ScreenRun.cs
@@ -10,48 +10,60 @@ namespace Screen.Application.EventHandlers
 
         public async Task OnEvent(ScreenRunAddedEvent @event)
         {
-            _logger.LogInformation("OnEvent: ScreenRunAddedEvent: ScreenId {Id}, ScreenRunId {ScreenRunId}", @event.Id, @event.ScreenRunId);
-
-            var screenRun = _mapper.Map<Domain.Entities.ScreenRun>(@event);
-            var screen = await _screenRepository.ReadScreenById(@event.Id);
-            screen.DeepLastUpdated = DateTime.UtcNow;
-
-            // Override screenRun.Id to be the same as screenRun.ScreenRunId 
-            // as @event.Id refers to ScreenId (Aggregate Id) which is auto-mapped by the mapper
-            // In MongoDb, we want to use ScreenRunId as the Id of the entity
-            screenRun.Id = @event.ScreenRunId;
-
-            // Set the ScreenId to be the same as the Aggregate Id to maintain a 
-            // relationship between Screen and ScreenRun
-            screenRun.ScreenId = @event.Id;
-
-            screenRun.DateCreated = DateTime.UtcNow;
-            screenRun.IsModified = false;
-
+            _logger.LogInformation("Processing ScreenRunAddedEvent - ScreenId: {ScreenId}, ScreenRunId: {ScreenRunId}",
+                @event.Id, @event.ScreenRunId);
             try
             {
-                await _screenRunRepository.CreateScreenRun(screenRun);
+                var screenRun = _mapper.Map<ScreenRun>(@event);
+                var screen = await _screenRepository.ReadScreenById(@event.Id)
+                             ?? throw new EventHandlerException(nameof(ScreenEventHandler),
+                                $"Screen not found for ID {@event.Id}", new Exception("EntityNotFound"));
+
+
+                screen.DeepLastUpdated = DateTime.UtcNow;
+
+                // Override screenRun.Id to be the same as screenRun.ScreenRunId 
+                // as @event.Id refers to ScreenId (Aggregate Id) which is auto-mapped by the mapper
+                // In MongoDb, we want to use ScreenRunId as the Id of the entity
+                screenRun.Id = @event.ScreenRunId;
+
+                // Set the ScreenId to be the same as the Aggregate Id to maintain a 
+                // relationship between Screen and ScreenRun
+                screenRun.ScreenId = @event.Id;
+
+                screenRun.DateCreated = DateTime.UtcNow;
+                screenRun.IsModified = false;
+
+
                 await _screenRepository.UpdateScreen(screen);
+                await _screenRunRepository.CreateScreenRun(screenRun);
             }
             catch (RepositoryException ex)
             {
-                throw new EventHandlerException(nameof(EventHandler), "Error occurred while creating screen run for ScreenRunAddedEvent", ex);
+                throw new EventHandlerException(nameof(ScreenEventHandler),
+                    "Error while creating screen run", ex);
             }
         }
 
         public async Task OnEvent(ScreenRunUpdatedEvent @event)
         {
-            _logger.LogInformation("OnEvent: ScreenRunUpdatedEvent: ScreenId {Id}, ScreenRunId {ScreenRunId}", @event.Id, @event.ScreenRunId);
-            //_logger.LogInformation("OnEvent: ScreenRunUpdatedEventJSON: {event}", System.Text.Json.JsonSerializer.Serialize(@event));
+            _logger.LogInformation("Processing ScreenRunUpdatedEvent - ScreenId: {ScreenId}, ScreenRunId: {ScreenRunId}",
+                @event.Id, @event.ScreenRunId);
 
             try
             {
-                var existingScreenRun = await _screenRunRepository.ReadScreenRunById(@event.ScreenRunId);
+                var existingScreenRun = await _screenRunRepository.ReadScreenRunById(@event.ScreenRunId)
+                                        ?? throw new EventHandlerException(nameof(ScreenEventHandler),
+                                           $"ScreenRun not found for ID {@event.ScreenRunId}", new Exception("EntityNotFound"));
+
+                var screen = await _screenRepository.ReadScreenById(@event.Id)
+                             ?? throw new EventHandlerException(nameof(ScreenEventHandler),
+                                $"Screen not found for ID {@event.Id}", new Exception("EntityNotFound"));
+
+                screen.DeepLastUpdated = DateTime.UtcNow;
 
                 var screenRun = _mapper.Map<ScreenRun>(existingScreenRun);
-                //_logger.LogInformation("OnEvent: ScreenRunUpdatedEvent: ExistingScreenRunJSON: {existingScreenRun}", System.Text.Json.JsonSerializer.Serialize(existingScreenRun));
-                
-                
+
                 _mapper.Map(@event, screenRun);
 
                 screenRun.Id = @event.ScreenRunId;
@@ -61,42 +73,41 @@ namespace Screen.Application.EventHandlers
                 screenRun.CreatedById = existingScreenRun.CreatedById;
                 screenRun.DateCreated = existingScreenRun.DateCreated;
 
-                //_logger.LogInformation("OnEvent: ScreenRunUpdatedEvent: ScreenRunToUpdateJSON: {screenRun}", System.Text.Json.JsonSerializer.Serialize(screenRun));
-
-                var screen = await _screenRepository.ReadScreenById(@event.Id);
-                screen.DeepLastUpdated = DateTime.UtcNow;
 
                 await _screenRunRepository.UpdateScreenRun(screenRun);
                 await _screenRepository.UpdateScreen(screen);
-                
+
             }
             catch (RepositoryException ex)
             {
-                throw new EventHandlerException(nameof(EventHandler), $"Error occurred while updating screen run {@event.ScreenRunId} for ScreenRunUpdatedEvent", ex);
+                throw new EventHandlerException(nameof(ScreenEventHandler),
+                                    "Error while updating screen run", ex);
             }
         }
 
         public async Task OnEvent(ScreenRunDeletedEvent @event)
         {
-            _logger.LogInformation("OnEvent: ScreenRunDeletedEvent: {Id}", @event.Id);
-            var existingScreenRun = await _screenRunRepository.ReadScreenRunById(@event.ScreenRunId);
-
-            if (existingScreenRun == null)
-            {
-                throw new EventHandlerException(nameof(EventHandler), $"Error occurred while deleting screen run {@event.ScreenRunId} for ScreenRunDeletedEvent", new Exception("Screen run not found"));
-            }
-
-            var screen = await _screenRepository.ReadScreenById(existingScreenRun.ScreenId);
-            screen.DeepLastUpdated = DateTime.UtcNow;
+            _logger.LogInformation("Processing ScreenRunDeletedEvent - ScreenRunId: {ScreenRunId}", @event.ScreenRunId);
 
             try
             {
+                var existingScreenRun = await _screenRunRepository.ReadScreenRunById(@event.ScreenRunId)
+                                        ?? throw new EventHandlerException(nameof(ScreenEventHandler),
+                                           $"ScreenRun not found for ID {@event.ScreenRunId}", new Exception("EntityNotFound"));
+
+                var screen = await _screenRepository.ReadScreenById(existingScreenRun.ScreenId)
+                             ?? throw new EventHandlerException(nameof(ScreenEventHandler),
+                                $"Screen not found for ID {existingScreenRun.ScreenId}", new Exception("EntityNotFound"));
+
+                screen.DeepLastUpdated = DateTime.UtcNow;
+
                 await _screenRunRepository.DeleteScreenRun(@event.ScreenRunId);
                 await _screenRepository.UpdateScreen(screen);
             }
             catch (RepositoryException ex)
             {
-                throw new EventHandlerException(nameof(EventHandler), $"Error occurred while deleting screen run {@event.ScreenRunId} for ScreenRunDeletedEvent", ex);
+                throw new EventHandlerException(nameof(ScreenEventHandler),
+                    "Error while deleting screen run", ex);
             }
         }
 
